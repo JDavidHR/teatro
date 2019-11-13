4fee1f1e1d3f95ff3af361048c1d26dc46c362fd
JDavidHR <56202293+JDavidHR@users.noreply.github.com>
2019-11-13 14:36:23 -0500

    login teatro

---
+++ login_teatro.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+  <link rel="stylesheet" type="text/css" href="css/style_login.css">
+
+</head>
+<body>
+      <section class="login-block">
+          <div class="container">
+        <div class="row">
+          <div class="col-md-4 login-sec">
+              <h2 class="text-center">Bienvenido</h2>
+              <form class="login-form">
+        <div class="form-group">
+          <label for="exampleInputEmail1" class="text-uppercase">Documento</label>
+          <input type="text" class="form-control" placeholder="No. Documento">
+          
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1" class="text-uppercase">Password</label>
+          <input type="password" class="form-control" placeholder="Password">
+        </div>
+        
+        
+          <div class="form-check">
+          <button type="submit" class="btn btn-login float-right">Iniciar sesión</button>
+        </div>
+        
+      </form>
+      
+          </div>
+          <div class="col-md-8 banner-sec">
+
+                  
+          
+            <img class="d-block img-fluid" src="image/teatro_login.jpg" alt="First slide">
+            
+              <div class="banner-text">
+                  <h2 style="color: white;">Proximo Evento!</h2>
+                  <p style="color: white;">Rapunzel y el colorin colorado de nuevo en acción</p>
+              </div>  
+    
+          </div>
+        </div>
+      </div>
+      </section>
+</body>
+</html>
