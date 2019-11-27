257d5296ea211ff74b2366b0b1492a86a66ba3a8
Juan David Hoyos <simond202020@gmail.com>
2019-11-27 16:29:26 -0500

    Compra full

---
+++ Controlador/compra_funcion.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Procesando</title>
+  <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+  <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+<!-- Llamado de css -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Candal">
+  <link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+    <div class="col-lg-offset-3 col-lg-6">
+<?php
+    //Comprobar que los campos no esten vacios   
+    if(isset($_POST['enviar']) && !empty($_POST['fecha']) &&
+        !empty($_POST['tipo_teatro']) && !empty($_POST['tipo_fun']) &&
+        !empty($_POST['tipo_cliente']))
+    {
+        //Llamar al archivo MySQL
+        require_once '../Modelo/MySQL.php';
+            
+        //Recuperar datos del formulario (crear_cita.php)    
+        $fecha = $_POST['fecha'];
+        $fecha = date('Y-m-d H:i:s');
+        $tipo_teatro = $_POST['tipo_teatro'];
+        $tipo_fun = $_POST['tipo_fun'];
+        $tipo_cliente = $_POST['tipo_cliente'];
+        
+
+        
+        //Nuevo archivo MySQL y se llama a la funcion conectar()
+        $mysql = new MySQL;
+        $mysql->conectar();
+
+        
+
+        
+        //Si se efectuo correctamente la consulta se redirige al index
+        if($tipo_fun == 1){
+          
+
+        //Variable para efectuar la consulta SQL, en este caso, insertar datos en la tabla citas
+        $compra = $mysql->efectuarConsulta("insert into teatro.funciones(fecha_hora,Teatro_id,Tipo_funcion_id,Tipo_cliente_id,precio) VALUES ('".$fecha."','".$tipo_teatro."','".$tipo_fun."','".$tipo_cliente."',10000)"); 
+
+          if($compra){
+           //impresion de mensajes personalizados
+           echo "<div class=\"alert alert-success alert-dismissible\"><a href=\"../ver_cita.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Felicidades!</strong>Compra exitosa.</div>";
+           //redireccion
+           header( "refresh:3;url=../index.php" ); 
+        }
+        //Sino da mensaje de error 
+        else {
+            //mensaje de error personalizado
+           echo "<div class=\"alert alert-warning alert-dismissible\"><a href=\"../crear_cita.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Alerta!</strong>No se ha podido realizar la compra.</div>";
+           //redireccion
+           header( "refresh:3;url=../compra.php" );      
+        }
+      }
+        if($tipo_fun == 2){
+          
+
+        //Variable para efectuar la consulta SQL, en este caso, insertar datos en la tabla citas
+        $compra = $mysql->efectuarConsulta("insert into teatro.funciones(fecha_hora,Teatro_id,Tipo_funcion_id,Tipo_cliente_id,precio) VALUES ('".$fecha."','".$tipo_teatro."','".$tipo_fun."','".$tipo_cliente."',15000)"); 
+
+            if($compra){
+           //impresion de mensajes personalizados
+           echo "<div class=\"alert alert-success alert-dismissible\"><a href=\"../ver_cita.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Felicidades!</strong>Compra exitosa.</div>";
+           //redireccion
+           header( "refresh:3;url=../index.php" ); 
+        }
+        //Sino da mensaje de error 
+        else {
+            //mensaje de error personalizado
+           echo "<div class=\"alert alert-warning alert-dismissible\"><a href=\"../crear_cita.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Alerta!</strong>No se ha podido realizar la compra.</div>";
+           //redireccion
+           header( "refresh:3;url=../compra.php" );      
+        }
+      }
+        
+    }
+    
+?>
+  </div>
+  <!-- Llamado de los respectivos scripts -->
+  <script src="../js/jquery.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
--- comprar.php
+++ comprar.php
@@ -1,3 +1,17 @@
+<?php
+//llamado al archivo MySQL
+require_once 'Modelo/MySQL.php';
+//nueva "consulta"
+$mysql = new MySQL;
+//funcion conectar
+$mysql->conectar(); 
+
+$tipo_teatro  =  $mysql->efectuarConsulta ( " SELECT  teatro.tipo_teatro.id, teatro.tipo_teatro.tipo from tipo_teatro " );
+$tipo_funcion  =  $mysql->efectuarConsulta ( " SELECT  teatro.tipo_funcion.id, teatro.tipo_funcion.Nombre as tipo_fun from tipo_funcion ");
+$tipo_cliente  =  $mysql->efectuarConsulta ( " SELECT  teatro.tipo_cliente.id, teatro.tipo_cliente.Nombre from tipo_cliente " );
+
+$mysql->desconectar(); 
+?>
 <!DOCTYPE html>
 <html>
 
@@ -12,33 +26,60 @@
 <body>
     <div class="container" id="advanced-search-form">
         <h2>Compra del Ticket</h2>
-        <center><form>
+        <center><form method="Post" action="Controlador/compra_funcion.php">
             <div class="form-group">
-                <label for="first-name">Documento Usuario</label>
-                <input type="text" class="form-control" placeholder="No. Documento" id="first-name">
+                <label for="first-name">Fecha</label>
+                <input type="Date" class="form-control" placeholder="Fecha" id="first-name" name="fecha">
             </div>
+
             <div class="form-group">
-                <label for="first-name">Tarjeta Credito</label>
-                <input type="text" class="form-control" placeholder="No. Tarjeta" id="first-name">
-            </div>
+                  <label class="col-md-12">Teatro</label>
+                  <div class="col-sm-12">
+                      <select class="form-control form-control-line" name="tipo_teatro">
+                        <?php 
+                         //se recorre el resultado de la consutla de estado civil
+                        while ($resultado= mysqli_fetch_assoc($tipo_teatro)){
+                           //se imprime los resultados
+                           ?> 
+                        <option value="<?php echo $resultado['id']?>"><?php echo $resultado['tipo']?></option>  
+                        <?php }?>
+                    </select>
+                  </div>
+                </div>
+
             <div class="form-group">
-                <label for="last-name">Obra</label>
-                <select class="form-control">
-                    <option disabled="">Seleccione</option>
-                    <option>Romeo y Julieta</option>
-                    <option>La casa de Bernarda Alba</option>
-                    <option>Sueño de una noche de verano</option>
-                </select>
-                
-            </div>
+                  <label class="col-md-12">Tipo Funcion</label>
+                  <div class="col-sm-12">
+                      <select class="form-control form-control-line" name="tipo_fun">
+                        <?php 
+                         //se recorre el resultado de la consutla de estado civil
+                        while ($resultado= mysqli_fetch_assoc($tipo_funcion)){
+                           //se imprime los resultados
+                           ?> 
+                        <option value="<?php echo $resultado['id']?>"><?php echo $resultado['tipo_fun']?></option>  
+                        <?php }?>
+                    </select>
+                  </div>
+                </div>
+            
             <div class="form-group">
-                <label for="country">Hora</label>
-                <input type="time" class="form-control" placeholder="Hora" id="country">
-            </div>
+                  <label class="col-md-12">Tipo Cliente</label>
+                  <div class="col-sm-12">
+                      <select class="form-control form-control-line" name="tipo_cliente">
+                        <?php 
+                         //se recorre el resultado de la consutla de estado civil
+                        while ($resultado= mysqli_fetch_assoc($tipo_cliente)){
+                           //se imprime los resultados
+                           ?> 
+                        <option value="<?php echo $resultado['id']?>"><?php echo $resultado['Nombre']?></option>  
+                        <?php }?>
+                    </select>
+                  </div>
+                </div>
             
             
             <div class="clearfix"></div>
-            <button type="submit" class="btn btn-info btn-lg btn-responsive" id="search"> Comprar</button>
+            <button type="submit" class="btn btn-info btn-lg btn-responsive" id="search" name="enviar"> Comprar</button>
         </form></center>
     </div>
 </body>
