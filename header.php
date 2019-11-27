4ea60cac70873893e31f0ee418d510a750815636
Juan David Hoyos <simond202020@gmail.com>
2019-11-27 14:32:10 -0500

    modificacion se agrego el boton de funciones en el header

--- header.php
+++ header.php
@@ -62,7 +62,7 @@
                                 <a class="nav-link dropdown-toggle" href="" id="dropdown1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Funciones</a>
                                 <div class="dropdown-menu " aria-labelledby="dropdown1">
                                   <a class="dropdown-item btn" href="#">Ingresar nueva Funcion</a>
-                                  <a class="dropdown-item btn" href="#">Ver Funciones</a>
+                                  <a class="dropdown-item btn" href="ver_funcion.php">Ver Funciones</a>
                                   <a class="dropdown-item btn" href="#">Ver Funciones inactivas</a>
                                 </div>
                             </li>
