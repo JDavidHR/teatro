62887a480d76bfb74a8e407d054f67dcb42e8466
Natalia AV <nagudelo@iegabo.edu.co>
2019-11-27 14:45:16 -0500

    modificación idioma datatable y arreglo de grilla

--- listado_personajes.php
+++ listado_personajes.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Teatro Cotecnova - Ver obras</title>
+  <title>Teatro Cotecnova - Obras inactivas</title>
   <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
   <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
 
@@ -32,14 +32,22 @@
         include("header_index.php");
     ?>
     <?php 
+        $id = $_GET['id'];
         //llamado al archivo MySQL
         require_once 'Modelo/MySQL.php';
         //nueva "consulta"
         $mysql = new MySQL;
         //funcion conectar
-        $mysql->conectar();    
-         //respectivas variables donde se llama la función consultar, se incluye la respectiva consulta
-        $consulta = $mysql->efectuarConsulta("SELECT teatro.obra.id as idObra, teatro.obra.nombre as nombre, teatro.autor.nombre as autor, teatro.tipo_obra.nombre as tipo, teatro.director.nombre as director from teatro.obra join teatro.autor on teatro.obra.Autor_id = teatro.autor.id join teatro.tipo_obra on teatro.tipo_obra.id = teatro.obra.Tipo_obra_id join teatro.director on teatro.obra.Director_id = teatro.director.id where estado = 1");
+        $mysql->conectar();
+        //consulta de toda la informacion
+        $consulta = $mysql->efectuarConsulta("SELECT teatro.obra_x_personajes.Obra_id as id_obra, teatro.obra.nombre as nombre, teatro.personajes.id as id_personaje, teatro.personajes.nombre as personajes, teatro.personajes.descripcion as descripcion, teatro.actores.nombre as actor, teatro.tipo_papel.nombre as papel from teatro.obra_x_personajes join teatro.obra on teatro.obra.id = teatro.obra_x_personajes.Obra_id join teatro.personajes on teatro.personajes.id = teatro.obra_x_personajes.Personajes_id join teatro.actores on teatro.actores.id = teatro.personajes.Actores_id join teatro.tipo_papel on teatro.tipo_papel.id = teatro.actores.Tipo_papel_id where teatro.obra_x_personajes.Obra_id = ".$id."");  
+       
+        
+        $seleccionObra = $mysql->efectuarConsulta("SELECT 
+        teatro.obra.nombre as nombre2 FROM obra WHERE teatro.obra.id = ".$id."");  
+        while ($resultado2 = mysqli_fetch_assoc($seleccionObra)){
+          $nombreObra2 = $resultado2['nombre2'];
+        }
         //funcion desconectar
         $mysql->desconectar();    
         ?>
@@ -51,7 +59,7 @@
             <div class="col-md-3 col-sm-3">
             <h2 class="ser-title">Bienvenido</h2>
             <hr class="botm-line">
-            <p>Obras actualmente disponibles</p>
+            <p>Listado de personajes</p>
             <p>Todos los datos mostrados son los suministrados por el teatro ser&aacute;n de uso aplicativo.</p>
             </div>
         <div class="col-md-9 col-sm-9">
@@ -62,11 +70,10 @@
                 <table class="table table-hover" id="ver_obra">
                     <thead>
                         <tr>
-                            <th scope="col">Nombre de la obra</th>
-                            <th scope="col">Autor</th>
-                            <th scope="col">Genero</th>
-                            <th scope="col">Director</th>
-                            <th scope="col">Personajes</th>
+                            <th scope="col">Personaje</th>
+                            <th scope="col">Descripción</th>
+                            <th scope="col">Actor</th>
+                            <th scope="col">Papel</th>
                             <th scope="col">Editar</th>
                             <th scope="col">Eliminar</th>
                         </tr>
@@ -75,24 +82,21 @@
                     <!-- Llamado al ciclo while donde vamos a recorrer un array asociativo con la consulta declarada anteriormente -->
                     <?php 
                         while ($resultado= mysqli_fetch_assoc($consulta)){  
-                        $idObra = $resultado['idObra'];                       
+                        $idPersonaje = $resultado['id_personaje'];                       
                     ?>
                     
                         <tr>
                             <!-- Se traen los datos y se imprimen en las opciones del select -->
-                            <td><?php echo $resultado['nombre'] ?></td>
-                            <td><?php echo $resultado['autor'] ?></th>
-                            <td><?php echo $resultado['tipo'] ?></td>                      
-                            <td><?php echo $resultado['director'] ?></td>
+                            <td><?php echo $resultado['personajes'] ?></td>
+                            <td><?php echo $resultado['descripcion'] ?></th>
+                            <td><?php echo $resultado['actor'] ?></td>                      
+                            <td><?php echo $resultado['papel'] ?></td>
                             <td>
-                                <a href="listado_personajes.php?id=<?php echo $idObra; ?>" class="btn btn-info " name="enviar">Listado</a>
-                            </td>
-                            <td>
-                                <a href="editar_obra.php?id=<?php echo $idObra; ?>" class="btn btn-success " name="enviar">Editar</a>
+                                <a href="editar_personaje.php?id=<?php echo $id_personaje; ?>" class="btn btn-success " name="enviar">Editar</a>
                             </td>
                             <td> 
-                                <a href="eliminar_obra.php?id=<?php echo $idObra ?>" class="btn btn-danger" name="eliminar">Eliminar</a>
-                            </td>
+                                <a href="eliminar_personaje.php?id=<?php echo $id_personaje; ?>" class="btn btn-danger" name="eliminar">Eliminar</a>
+                            </td> 
                         </tr>
                         <?php
                             }
