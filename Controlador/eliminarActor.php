50364f4fec52a99ff90098b82e8c9e816f21c773
Natalia AV <nagudelo@iegabo.edu.co>
2019-11-27 20:16:59 -0500

    activar actor, eliminar

---
+++ Controlador/eliminarActor.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Teatro Cotecnova - Eliminar actor</title>
+  <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+  <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+  <!-- Llamado de css -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Candal">
+  <link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+    <div class="col-lg-offset-3 col-lg-6">
+<?php
+//condicion para comprobar si los campos están declarados anteriormente y si no estan vacíos
+if(isset($_GET['id']) && !empty($_GET['id'])){
+    
+    //lamado al archivo MySQL
+    require_once '../Modelo/MySQL.php';
+    
+    //declaracion de variables con sus respectivas asignaciones
+    $idActor = $_GET['id'];    
+    //nueva "archivo" MySQL
+    $mysql = new MySQL;
+    //llamado a funcion conectar
+    $mysql->conectar();
+    
+    //variable que ejecutara la funcion consulta, pero en este caso, sera un eliminar usuario actualizando su estado 1. Activo 2.Inactivo
+    $ActualizarEstado = $mysql->efectuarConsulta("update actores set estado = 0 where id =".$idActor.""); 
+    //Desconecto la conexion de la bD
+    $mysql->desconectar(); 
+    //decision para comprobar si se ejecuto, se redirige al index principal
+    if($ActualizarEstado){
+      //impresion de mensajes personalizados
+       echo "<div class=\"alert alert-success alert-dismissible\"><a href=\"../ver_actor.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Felicidades!</strong> El actor ha sido inhabilitado correctamente.</div>";
+       //personalizacion
+       header( "refresh:3;url=../ver_personaje.php" ); 
+    } else {
+        //mensaje de error personalizado
+        echo "<div class=\"alert alert-warning alert-dismissible\"><a href=\"../ver_actor.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Alerta!</strong> No se ha podido inhabilitar el actor.</div>";
+        //redireccion
+        header( "refresh:3;url=../eliminar_personaje.php" ); 
+    }    
+}else{
+  //mensaje personalizado
+    echo "<div class=\"alert alert-warning alert-dismissible\"><a href=\"../ver_personaje.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Alerta!</strong>No se han enviado el ID del actor.</div>";
+    //redireccion
+    header( "refresh:3;url=../ver_personaje.php" ); 
+}
+?>
+  </div>
+  <!-- Llamado de los respectivos scripts -->
+  <script src="../js/jquery.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ eliminar_actor.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Teatro Cotecnova - Eliminar actor</title>
+  <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+  <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Candal">
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/style2.css">
+  <!-- =======================================================
+    Theme Name: Medilab
+    Theme URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
+    Author: BootstrapMade.com
+    Author URL: https://bootstrapmade.com
+  ======================================================= -->
+</head>
+
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+  <div id="container">
+    <?php
+    session_start();
+    if(isset($_SESSION['tipousuario'])){
+        if($_SESSION['tipousuario'] == 1){ //Sesion como medico
+            include("header_index.php");
+    ?>
+    </div>  
+    <?php
+            //trae id de la obra 
+            $id = $_GET['id'];
+            //llamado al archivo MySQL
+            require_once 'Modelo/MySQL.php';
+            //nueva "consulta"
+            $mysql = new MySQL;
+            //funcion conectar
+            $mysql->conectar();
+            //consulta de toda la informacion
+            $seleccionInformacion = $mysql->efectuarConsulta("select teatro.actores.id as id_actor, teatro.actores.nombre from actores where id = ".$id."");
+            //funcion desconectar
+            $mysql->desconectar();    
+            while ($resultado= mysqli_fetch_assoc($seleccionInformacion)){
+                $idActor = $resultado['id_actor'];
+                $nombre = $resultado['nombre'];
+            }  
+    ?>
+    <!--service-->
+    <section id="service" class="section-padding">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-4">
+            <h2 class="ser-title">Bienvenido</h2>
+            <hr class="botm-line">
+            <p>Bienvenid@ al formulario de borrar.</p>
+            <p>Todos los datos pedidos ser&aacute;n de uso aplicativo.</p>
+          </div>
+          <div class="col-md-8 col-sm-8">
+          <div class="card">
+            <!-- Tab panes -->
+            <div class="card-body">
+              <form class="form-horizontal form-material" method="post" action="Controlador/eliminarActor.php?id=<?php echo $id;?>">
+                  <div class="form-group">
+                    <label class="col-sm-12">Id del actor</label>
+                    <div class="col-md-12">
+                      <!-- Se traen los datos y se imprimen en las opciones del select -->
+                      <input type="text" disabled="" value="<?php echo $idActor?>" class="form-control form-control-line">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label class="col-sm-12">Nombre del actor</label>            
+                    <div class="col-md-12">
+                      <!-- Se traen los datos y se imprimen en las opciones del select -->
+                      <input type="text" disabled=" "value="<?php echo $nombre?>" class="form-control form-control-line">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label class="col-sm-12">¿Esta seguro de eliminar este actor?</label>
+                    <br>
+                    <br>
+                    <div class="col-sm-2 col-md-2">
+                      <button class="btn btn-success" >Eliminar</button>
+                    </div>
+                    <div class="col-sm-10 col-md-4">
+                        <a href="ver_actor.php" class="btn btn-danger">Cancelar</a>
+                    </div>
+                  </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ service-->
+  <!--footer-->
+  <div id="footer">
+  <?php
+            include("footer.php");
+  ?>
+  </div>
+  <!--/ footer-->
+  <?php
+        }else{
+            header( "refresh:0;url=index.php" );   
+        }
+    }else{
+        header( "refresh:0;url=login.php" );    
+    }
+    ?>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery.easing.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/custom.js"></script>
+  <script src="contactform/contactform.js"></script>
+
+</body>
+
+</html>
